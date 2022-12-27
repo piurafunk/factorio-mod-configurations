18aacb38b2fa54daf5befc7c28586f7cdab97110
James Funk <piurafunk@gmail.com>
2022-12-26 23:41:20 -0700

    Initial commit
    Introduces the first functional edition of the mod. Allows configuruing
    the values of:
    
    - Space transport belt speed
    - Space underground belt distance
    - Space pipe to ground
    - Space assembling machine speed

---
+++ data.lua
@@ -0,0 +1,15 @@
+local magic_constant = (1+1/3)/10 / 64 -- Whatever the math is, multiplying by this translates "items/second" to the game engine's expected value
+
+-- Space belt speed
+data.raw["transport-belt"]["se-space-transport-belt"].speed = settings.startup["space-exploration-configuration-space-belt-speed"].value * magic_constant
+data.raw["underground-belt"]["se-space-underground-belt"].speed = settings.startup["space-exploration-configuration-space-belt-speed"].value * magic_constant
+data.raw["splitter"]["se-space-splitter"].speed = settings.startup["space-exploration-configuration-space-belt-speed"].value * magic_constant
+
+-- Space underground belt distance
+data.raw["underground-belt"]["se-space-underground-belt"].max_distance = settings.startup["space-exploration-configuration-underground-space-belt-max-distance"].value
+
+-- Space pipe to ground
+data.raw["pipe-to-ground"]["se-space-pipe-to-ground"].fluid_box.pipe_connections[2].max_underground_distance = settings.startup["space-exploration-configuration-space-pipe-to-ground-max-distance"].value
+
+-- Space assembling machine speed
+data.raw["assembling-machine"]["se-space-assembling-machine"].crafting_speed = settings.startup["space-exploration-configuration-space-assembling-machine-speed"].value
---
+++ settings.lua
@@ -0,0 +1,33 @@
+data:extend({
+    {
+        type = "int-setting",
+        name = "space-exploration-configuration-space-belt-speed",
+        setting_type = "startup",
+        default_value = 60,
+        order = "aa"
+    },
+    {
+        type = "int-setting",
+        name = "space-exploration-configuration-underground-space-belt-max-distance",
+        setting_type = "startup",
+        default_value = 10,
+        minimum_value = 0,
+        order = "ab"
+    },
+    {
+        type = "int-setting",
+        name = "space-exploration-configuration-space-pipe-to-ground-max-distance",
+        setting_type = "startup",
+        default_value = 10,
+        minimum_value = 1,
+        order = "ba"
+    },
+    {
+        type = "double-setting",
+        name = "space-exploration-configuration-space-assembling-machine-speed",
+        setting_type = "startup",
+        default_value = 2.0,
+        minimum_value = 0,
+        order = "ca"
+    }
+})
