a7fd47c7d8e415ff9ba3360f8bf2450dfd79deff
James Funk <piurafunk@gmail.com>
2024-03-31 20:18:45 -0600

    Add space exploration compatability

--- data.lua
+++ data.lua
@@ -1,30 +1,15 @@
-local magic_constant = (1+1/3)/10 / 64 -- Whatever the math is, multiplying by this translates "items/second" to the game engine's expected value
+local magic_constant = (1 + 1 / 3) / 10 / 64 -- Whatever the math is, multiplying by this translates "items/second" to the game engine's expected value
 local mod_prefix = 'mod-configurations-'
 
--- Space belt speed
-data.raw["transport-belt"]["se-space-transport-belt"].speed = settings.startup[mod_prefix .. "space-belt-speed"].value * magic_constant
-data.raw["underground-belt"]["se-space-underground-belt"].speed = settings.startup[mod_prefix .. "space-belt-speed"].value * magic_constant
-data.raw["splitter"]["se-space-splitter"].speed = settings.startup[mod_prefix .. "space-belt-speed"].value * magic_constant
+-- Create table of mods we support
+local mods = {{
+    name = 'space-exploration',
+    file = 'space_exploration.lua'
+}}
 
--- Space underground belt distance
-data.raw["underground-belt"]["se-space-underground-belt"].max_distance = settings.startup[mod_prefix .. "underground-space-belt-max-distance"].value
-data.raw["underground-belt"]["se-deep-space-underground-belt-black"].max_distance = settings.startup[mod_prefix .. "underground-deep-space-belt-max-distance"].value
-data.raw["underground-belt"]["se-deep-space-underground-belt-blue"].max_distance = settings.startup[mod_prefix .. "underground-deep-space-belt-max-distance"].value
-data.raw["underground-belt"]["se-deep-space-underground-belt-cyan"].max_distance = settings.startup[mod_prefix .. "underground-deep-space-belt-max-distance"].value
-data.raw["underground-belt"]["se-deep-space-underground-belt-green"].max_distance = settings.startup[mod_prefix .. "underground-deep-space-belt-max-distance"].value
-data.raw["underground-belt"]["se-deep-space-underground-belt-magenta"].max_distance = settings.startup[mod_prefix .. "underground-deep-space-belt-max-distance"].value
-data.raw["underground-belt"]["se-deep-space-underground-belt-red"].max_distance = settings.startup[mod_prefix .. "underground-deep-space-belt-max-distance"].value
-data.raw["underground-belt"]["se-deep-space-underground-belt-white"].max_distance = settings.startup[mod_prefix .. "underground-deep-space-belt-max-distance"].value
-data.raw["underground-belt"]["se-deep-space-underground-belt-yellow"].max_distance = settings.startup[mod_prefix .. "underground-deep-space-belt-max-distance"].value
-
--- Space pipe to ground
-data.raw["pipe-to-ground"]["se-space-pipe-to-ground"].fluid_box.pipe_connections[2].max_underground_distance = settings.startup[mod_prefix .. "space-pipe-to-ground-max-distance"].value
-
--- Space assembling machine speed
-data.raw["assembling-machine"]["se-space-assembling-machine"].crafting_speed = settings.startup[mod_prefix .. "space-assembling-machine-speed"].value
-
--- Mechanical Facility
-local allowCoreFragmentProcessingInMechanicalLaboratory = settings.startup[mod_prefix .. "allow-core-fragment-processing-in-mechanical-laboratory"].value
-if allowCoreFragmentProcessingInMechanicalLaboratory then
-    table.insert(data.raw['assembling-machine']['se-space-mechanical-laboratory'].crafting_categories, 'core-fragment-processing')
+-- Iterate over supported mods, and if they are installed, load their respective configuration file
+for _, mod in pairs(mods) do
+    if mods[mod.name] then
+        require('compatability/' .. mod.file)
+    end
 end
